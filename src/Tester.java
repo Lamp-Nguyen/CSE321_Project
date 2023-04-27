7635ada344bf95c24c78fc1e99a2443635700c57
Lamp-Nguyen <lamnguyenmai02@gmail.com>
2023-04-27 11:15:22 -0400

    Updated tester and added comments

--- src/Tester.java
+++ src/Tester.java
@@ -1,34 +1,76 @@
+import java.util.Scanner;
 
 public class Tester {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String input;
+		int noRerolls = 0;
 		YahtzeeMain test = new YahtzeeMain();
-		test.reroll("1 2 3 4 5");
-		System.out.println(test.getDies());
-		testLowerSection(test);
 		
-		test.reroll("2 4 5");
-		System.out.println(test.getDies());
-		testLowerSection(test);
-		
-		test.reroll("5 4 1");
-		System.out.println(test.getDies());
-		testLowerSection(test);
-		
-		test.reroll("3 5 4");
-		System.out.println(test.getDies());
-		testLowerSection(test);
+		// 13 rounds
+		for (int i = 0; i < 13; i++) {
+			System.out.println("Round no: " + (i + 1));
+			System.out.println("-----------------------------");
+			// while the user still has rerolls
+			while (noRerolls < 3) {
+				
+				System.out.println(test.getDies());
+				testUpperSection(test);
+				System.out.println("-----------------------------");
+				testLowerSection(test);
+				System.out.println("-----------------------------");
+				
+				// if the user hasn't rerolled 2 times
+				if (noRerolls < 2) {
+					System.out.println("Enter the dies you want to reroll or enter your score: ");
+					input = sc.nextLine().trim();
+					// if the user keeps a score
+					if (input.equals("score")) {
+						noRerolls = 0;
+						test.reroll();
+						break;
+					// if the user enters a string of dies to reroll
+					} else if (input != ""){
+						noRerolls++;
+						test.reroll(input);
+					// if the user enters nothing
+					} else {
+						noRerolls++;
+						test.reroll();
+					}
+				// if the user used all their rerolls
+				} else {
+					noRerolls++;
+					System.out.println("You've run out of rolls, enter your score");
+					input = sc.nextLine().trim();
+				}
+			}
+			
+			noRerolls = 0;
+			System.out.println("-----------------------------");
+		}
+	}
+	
+	public static void testUpperSection(YahtzeeMain y) {
+		System.out.println("Aces: " + y.getDieSum(1));
+		System.out.println("Twos: " + y.getDieSum(2));
+		System.out.println("Threes: " + y.getDieSum(3));
+		System.out.println("Fours: " + y.getDieSum(4));
+		System.out.println("Fives: " + y.getDieSum(5));
+		System.out.println("Sixes: " + y.getDieSum(6));
 	}
 	
 	public static void testLowerSection(YahtzeeMain y) {
 		System.out.println("Three of a Kind: " + y.getSameDie());
-		System.out.println(" Four of a Kind: " + y.getSameDie());
-		System.out.println(" Full House: " + y.getFullHouse());
-		System.out.println(" Small Straight: " + y.getSmallStraight());
-		System.out.println(" Large Straight: " + y.getLargeStraight());
-		System.out.println(" Chance: " + y.getRollSum());
-		System.out.println(" Yahtzee: " + y.getYahtzee());
-		System.out.println(" Die counts: " + y.getCount());
+		System.out.println("Four of a Kind: " + y.getSameDie());
+		System.out.println("Full House: " + y.getFullHouse());
+		System.out.println("Small Straight: " + y.getSmallStraight());
+		System.out.println("Large Straight: " + y.getLargeStraight());
+		System.out.println("Chance: " + y.getRollSum());
+		System.out.println("Yahtzee: " + y.getYahtzee());
+		System.out.println("Die counts: " + y.getCount());
 	}
 	
 }
--- src/YahtzeeMain.java
+++ src/YahtzeeMain.java
@@ -3,9 +3,24 @@ import java.util.HashMap;
 
 public class YahtzeeMain {
 
+	//------------------------------------- Instance variables
+	
+	// Hashmap to store the dies in <String, Die> pairs (Ex: <"1", Die>, <"2", Die>)
 	private HashMap<String, Die> dies;
+	
+	// ArrayList to store the count of the dies of value n (Ex: If the user roll 2 aces,
+	// 2 twos and 1 three, the count list will be [2, 2, 1, 0, 0, 0]
 	private ArrayList<Integer> count;
 	
+	// Count for Yahtzee bonus
+	private int yahtzeeCount = 0;
+	
+	//------------------------------------- Methods
+	
+	/**
+	 * Main constructor for the class, initialize the HashMap,
+	 * ArrayList, and update the count
+	 */
 	public YahtzeeMain() {
 		dies = new HashMap<String, Die>();
 		count = new ArrayList<Integer>(6);
@@ -15,8 +30,27 @@ public class YahtzeeMain {
 		for (int i = 0; i < 6; i++) {
 			count.add(0);
 		}
+		updateCount();
 	}
 	
+	/**
+	 * Reroll all dies, updateCount is called to update the count ArrayList to match
+	 * the current turn
+	 */
+	public void reroll() {
+		for (int i = 1; i <= 5; i++) {
+			String idx = i + "";
+			dies.get(idx).roll();
+		}
+		updateCount();
+	}
+	
+	/**
+	 * Reroll specific dies, updateCount is called to update the count ArrayList to match
+	 * the current turn
+	 * @param dieNumbers the dies the players want to reroll, represented as
+	 * 	a string of integers seperated by spaces
+	 */
 	public void reroll(String dieNumbers) {
 		String[] diesToReroll = dieNumbers.trim().split(" ");
 		for (String str : diesToReroll) {
@@ -25,14 +59,22 @@ public class YahtzeeMain {
 		updateCount();
 	}
 	
+	/**
+	 * @return The String representation of the current dies
+	 */
 	public String getDies() {
 		String ret = "";
 		for (String key : dies.keySet()) {
-			ret += "Die " + key + ":" + dies.get(key).getValue() + " ";
+			ret += "[Die " + key + ": " + dies.get(key).getValue() + "]  |  ";
 		}
 		return ret;
 	}
 	
+	/**
+	 * Gets the current number of dies with the value dieValue
+	 * @param dieValue the value of the Die
+	 * @return the number of dies with the value dieValue
+	 */
 	public int dieCount(int dieValue) {
 		int count = 0;
 		for (Die die : dies.values())
@@ -41,15 +83,27 @@ public class YahtzeeMain {
 		return count;
 	}
 	
+	/**
+	 * Update the count ArrayList to match with the current turn
+	 */
 	public void updateCount() {
 		for (int i = 0; i < 6; i++)
 			count.set(i, dieCount(i + 1));
 	}
 	
+	/**
+	 * Get the total values of the dies with the value dieValue
+	 * @param dieValue the value of the die
+	 * @return the total values of the dies with the value dieValue
+	 */
 	public int getDieSum(int dieValue) {
 		return count.get(dieValue - 1) * dieValue;
 	}
 	
+	/**
+	 * Get the total values of all five dies
+	 * @return total values of all five dies
+	 */
 	public int getRollSum() {
 		int ret = 0;
 		for (int  i = 0; i < 6; i++) {
@@ -58,16 +112,28 @@ public class YahtzeeMain {
 		return ret;
 	}
 	
+	/**
+	 * Method for getting 3 or 4 of a kind
+	 * @return the roll total if the user gets 3 or 4 of a kind,
+	 * 0 otherwise 
+	 */
 	public int getSameDie() {
 		if (count.contains(3) || count.contains(4)) return getRollSum();
 		return 0;
 	}
-	
+	/**
+	 * Method for full house
+	 * @return 25 if the user get full house, 0 otherwise
+	 */
 	public int getFullHouse() {
 		if (count.contains(3) && count.contains(2)) return 25;
 		return 0;
 	}
 	
+	/**
+	 * Method for small straight
+	 * @return 30 if the user get a small straight, 0 otherwise
+	 */
 	public int getSmallStraight() {
 		int straights = 0;
 		for (int i = 0; i < 6; i++) {
@@ -81,6 +147,10 @@ public class YahtzeeMain {
 		return 0;
 	}
 	
+	/**
+	 * Method for large straight
+	 * @return 40 if the user get a large straight, 0 otherwise
+	 */
 	public int getLargeStraight() {
 		int straights = 0;
 		for (int i = 0; i < 6; i++) {
@@ -94,11 +164,20 @@ public class YahtzeeMain {
 		return 0;
 	}
 	
+	/**
+	 * Method for Yahtzee
+	 * @return return 50 if the user get a Yahtzee and increment the
+	 * Yahtzee bonus, 0 otherwise
+	 */
 	public int getYahtzee() {
-		if (count.contains(5)) return 50;
+		if (count.contains(5)) {
+			yahtzeeCount++;
+			return 50;
+		}
 		return 0;
 	}
 	
+	//Debug method for printing the count arraylist
 	public String getCount() {
 		return count.toString();
 	}
